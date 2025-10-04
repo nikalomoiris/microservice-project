f5914c6f1bb0831f1ace44f7aaaceeb7bd09abea
Nik Kalomoiris <nikalomoiris@gmail.com>
2025-10-04 06:00:35 -0400

    refactor(product-service): Use constructor injection in ProductController

--- services/product-service/src/main/java/nik/kalomiris/product_service/product/ProductController.java
+++ services/product-service/src/main/java/nik/kalomiris/product_service/product/ProductController.java
@@ -2,7 +2,6 @@ package nik.kalomiris.product_service.product;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
@@ -18,8 +16,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequestMapping("api/products")
 public class ProductController {
 
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+
+    ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product product){
