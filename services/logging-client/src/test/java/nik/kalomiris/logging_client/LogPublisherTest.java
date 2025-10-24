57077a2fd66ecd9bbff9f2ac3212bfdb3ff8381b
Nik Kalomoiris <nikalomoiris@gmail.com>
2025-10-24 09:15:45 -0400

    Remove unused InjectMocks import from LogPublisherTest

--- services/logging-client/src/test/java/nik/kalomiris/logging_client/LogPublisherTest.java
+++ services/logging-client/src/test/java/nik/kalomiris/logging_client/LogPublisherTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
